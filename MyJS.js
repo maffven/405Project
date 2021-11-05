b3a3f2a6b73dfcca3783d1bdce52f7206b6a3d49
imneell <73930342+maffven@users.noreply.github.com>
2021-11-05 16:01:07 +0300

    all files

---
+++ MyJS.js
@@ -0,0 +1,10 @@
+
+const password = document.querySelector('input[type="password"]');
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';
+});
+
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';
+});
+
