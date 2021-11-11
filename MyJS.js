e5e017d4f9b8ce4c52eaba644c429a87a4434ebe
imneell <73930342+maffven@users.noreply.github.com>
2021-11-11 19:50:40 +0300

    onBlur

--- MyJS.js
+++ MyJS.js
@@ -1,9 +1,4 @@
-function addHintTextFirstName(){
-  document.getElementById("herPass").placeholder = "Type your password here please";
-}
-function run(){
-  window.alert("don't stop");
-}
+
 
 function submitdata() { 
   var r = confirm('Press ok to confirm your submission.');
@@ -16,4 +11,22 @@ function submitdata() {
    }
    return false;         
 
+}
+
+function addHintTextPassword(){
+  document.getElementById("herPass").placeholder = "Enter a password longer than 6 characters";
+}
+function addHintTextFirstName(){
+  document.getElementById("firstName").placeholder = "Type your first name here please";
+}
+function addHintTextLastName(){
+  document.getElementById("lastName").placeholder = "Type your last name here please";
+}
+
+function addHintTextUserName(){
+  document.getElementById("username").placeholder = "Type your user name here please";
+}
+
+function changeColor(id){
+  document.getElementById(id).style.color="green";
 }
