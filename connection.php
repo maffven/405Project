16082dae3c5322cc65888e623b6f1bb2c60a3db3
aalsomali <79758265+aalsomali@users.noreply.github.com>
2021-11-14 21:08:19 +0300

    run this page to create User and workshop table

--- connection.php
+++ connection.php
@@ -3,13 +3,60 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
+try{
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new PDO ("mysql:host=$servername;dbname=Workshops",$username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+//create user table
+$sql = "CREATE TABLE `user` (
+  `Id` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Major` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Firstname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Lastname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Reason` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Agreement` bit(10) NOT NULL,
+  `Gender` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+  
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table User created successfully";
+
+//create workshop table
+$sql = "CREATE TABLE `workshop` (
+  `Title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Description` varchar(2000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Seats` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+  
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table Workshop created successfully";
+  
+  
+
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
 ?>
 
